4d7d4e17786085ba9c985c9c18b28f7f9eec4920
LaxmiKoushik N <itskoushik007@gmail.com>
2025-07-09 14:48:58 +0530

    app.js modified

--- app.js
+++ app.js
@@ -35,6 +35,10 @@ app.post("/upload", upload.single("file"), (req, res) => {
   });
 });
 
-app.listen(3000, () => {
+// app.listen(3000, () => {
+//   console.log("Server running on http://localhost:3000");
+// });
+
+app.listen(3000, "0.0.0.0", () => {
   console.log("Server running on http://localhost:3000");
 });
